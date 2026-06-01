6fcf4be3a73f9a528f571f0be4882eb7f65f698d
venkateshkumar-star <rvenkateshkumar2605@gmail.com>
2026-06-01 20:04:32 +0530

    Configure AWS provider and add VPC and EKS modules

--- icanio-project/terraform/main.tf
+++ icanio-project/terraform/main.tf
@@ -10,5 +10,77 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "ap-south-1"
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.8"
+
+  name = "devops-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.37.1"
+
+  cluster_name    = "devops-eks"
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    workers = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
