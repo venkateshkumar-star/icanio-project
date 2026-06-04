439d34d5beaa19a148a942ba4c42d1a6c95e029a
venkateshkumar-star <rvenkateshkumar2605@gmail.com>
2026-06-04 13:09:36 +0530

    Update app.py

--- icanio-project/app/app.py
+++ icanio-project/app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
+
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter(
+    'app_requests_total',
+    'Total App Request Count'
+)
 
 @app.route('/')
 def home():
+    REQUEST_COUNT.inc()
     return "DevOps Project Running 100% Successfully!"
+
+@app.route('/metrics')
+def metrics():
+    return Response(
+        generate_latest(),
+        mimetype='text/plain'
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
